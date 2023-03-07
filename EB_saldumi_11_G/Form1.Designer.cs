d785ff3114892e73c96c638d122f14dcce19243a
EdzusB <edzusbleikss1@gmail.com>
2023-03-07 08:31:49 +0200

    Programas papildinājumi

--- EB_saldumi_11_G/Form1.Designer.cs
+++ EB_saldumi_11_G/Form1.Designer.cs
@@ -57,31 +57,34 @@
             this.izmaksas4 = new System.Windows.Forms.TextBox();
             this.izmaksas5 = new System.Windows.Forms.TextBox();
             this.izmaksas6 = new System.Windows.Forms.TextBox();
+            this.checkatlaide = new System.Windows.Forms.CheckBox();
+            this.atlaidesbox = new System.Windows.Forms.TextBox();
+            this.gala_summa = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // daudzums
             // 
-            this.daudzums.Location = new System.Drawing.Point(337, 111);
+            this.daudzums.Location = new System.Drawing.Point(454, 99);
             this.daudzums.Name = "daudzums";
-            this.daudzums.Size = new System.Drawing.Size(100, 20);
+            this.daudzums.Size = new System.Drawing.Size(100, 23);
             this.daudzums.TabIndex = 0;
             this.daudzums.TextChanged += new System.EventHandler(this.daudzums_TextChanged);
             // 
             // aprekins
             // 
-            this.aprekins.Location = new System.Drawing.Point(396, 474);
+            this.aprekins.Location = new System.Drawing.Point(345, 478);
             this.aprekins.Name = "aprekins";
-            this.aprekins.Size = new System.Drawing.Size(75, 23);
+            this.aprekins.Size = new System.Drawing.Size(147, 23);
             this.aprekins.TabIndex = 1;
-            this.aprekins.Text = "Aprekinat";
+            this.aprekins.Text = "Aprēķināt atlikumu";
             this.aprekins.UseVisualStyleBackColor = true;
             this.aprekins.Click += new System.EventHandler(this.aprekins_Click);
             // 
             // daudzums_kg
             // 
-            this.daudzums_kg.Location = new System.Drawing.Point(382, 503);
+            this.daudzums_kg.Location = new System.Drawing.Point(371, 553);
             this.daudzums_kg.Name = "daudzums_kg";
-            this.daudzums_kg.Size = new System.Drawing.Size(100, 20);
+            this.daudzums_kg.Size = new System.Drawing.Size(100, 23);
             this.daudzums_kg.TabIndex = 2;
             this.daudzums_kg.TextChanged += new System.EventHandler(this.daudzums_kg_TextChanged);
             // 
@@ -92,7 +95,7 @@
             this.Nauda.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.Nauda.Location = new System.Drawing.Point(55, 97);
             this.Nauda.Name = "Nauda";
-            this.Nauda.Size = new System.Drawing.Size(276, 18);
+            this.Nauda.Size = new System.Drawing.Size(358, 24);
             this.Nauda.TabIndex = 4;
             this.Nauda.Text = "Ievadiet sev pieejamo naudas daudzumu!";
             // 
@@ -101,9 +104,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label2.Location = new System.Drawing.Point(309, 503);
+            this.label2.Location = new System.Drawing.Point(280, 551);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 18);
+            this.label2.Size = new System.Drawing.Size(85, 24);
             this.label2.TabIndex = 5;
             this.label2.Text = "Atlikums!";
             // 
@@ -113,11 +116,12 @@
             this.label3.BackColor = System.Drawing.Color.Maroon;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.label3.ForeColor = System.Drawing.SystemColors.Window;
-            this.label3.Location = new System.Drawing.Point(85, 9);
+            this.label3.Location = new System.Drawing.Point(322, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(640, 42);
+            this.label3.Size = new System.Drawing.Size(122, 54);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Iespejama daudzuma aprekinasana!!!";
+            this.label3.Text = "RIMI";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -125,7 +129,7 @@
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(62, 115);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(233, 13);
+            this.label4.Size = new System.Drawing.Size(307, 17);
             this.label4.TabIndex = 7;
             this.label4.Text = "(Daudzumu ievadiet ar komantu nevis punktu!!!)";
             // 
@@ -134,7 +138,7 @@
             this.check1.AutoSize = true;
             this.check1.Location = new System.Drawing.Point(58, 330);
             this.check1.Name = "check1";
-            this.check1.Size = new System.Drawing.Size(137, 17);
+            this.check1.Size = new System.Drawing.Size(176, 21);
             this.check1.TabIndex = 8;
             this.check1.Text = "Serenāde 17,63 eur/kg";
             this.check1.UseVisualStyleBackColor = true;
@@ -145,7 +149,7 @@
             this.check2.AutoSize = true;
             this.check2.Location = new System.Drawing.Point(58, 353);
             this.check2.Name = "check2";
-            this.check2.Size = new System.Drawing.Size(111, 17);
+            this.check2.Size = new System.Drawing.Size(140, 21);
             this.check2.TabIndex = 9;
             this.check2.Text = "Migle 7,49 Eur/kg";
             this.check2.UseVisualStyleBackColor = true;
@@ -156,7 +160,7 @@
             this.check3.AutoSize = true;
             this.check3.Location = new System.Drawing.Point(58, 376);
             this.check3.Name = "check3";
-            this.check3.Size = new System.Drawing.Size(147, 17);
+            this.check3.Size = new System.Drawing.Size(187, 21);
             this.check3.TabIndex = 10;
             this.check3.Text = "Rudzupuķe 12,19 Eur/kg";
             this.check3.UseVisualStyleBackColor = true;
@@ -167,7 +171,7 @@
             this.check4.AutoSize = true;
             this.check4.Location = new System.Drawing.Point(58, 399);
             this.check4.Name = "check4";
-            this.check4.Size = new System.Drawing.Size(122, 17);
+            this.check4.Size = new System.Drawing.Size(156, 21);
             this.check4.TabIndex = 11;
             this.check4.Text = "Gotiņa 10,69 eur/kg";
             this.check4.UseVisualStyleBackColor = true;
@@ -178,7 +182,7 @@
             this.check0.AutoSize = true;
             this.check0.Location = new System.Drawing.Point(58, 307);
             this.check0.Name = "check0";
-            this.check0.Size = new System.Drawing.Size(138, 17);
+            this.check0.Size = new System.Drawing.Size(177, 21);
             this.check0.TabIndex = 12;
             this.check0.Text = "Marmelāde 4,89 Eur/kg";
             this.check0.UseVisualStyleBackColor = true;
@@ -186,33 +190,33 @@
             // 
             // text0
             // 
-            this.text0.Location = new System.Drawing.Point(209, 305);
+            this.text0.Location = new System.Drawing.Point(241, 304);
             this.text0.Name = "text0";
-            this.text0.Size = new System.Drawing.Size(100, 20);
+            this.text0.Size = new System.Drawing.Size(100, 23);
             this.text0.TabIndex = 13;
             this.text0.TextChanged += new System.EventHandler(this.text1_TextChanged);
             // 
             // text2
             // 
-            this.text2.Location = new System.Drawing.Point(209, 350);
+            this.text2.Location = new System.Drawing.Point(241, 350);
             this.text2.Name = "text2";
-            this.text2.Size = new System.Drawing.Size(100, 20);
+            this.text2.Size = new System.Drawing.Size(100, 23);
             this.text2.TabIndex = 15;
             this.text2.TextChanged += new System.EventHandler(this.text3_TextChanged);
             // 
             // text3
             // 
-            this.text3.Location = new System.Drawing.Point(209, 376);
+            this.text3.Location = new System.Drawing.Point(241, 376);
             this.text3.Name = "text3";
-            this.text3.Size = new System.Drawing.Size(100, 20);
+            this.text3.Size = new System.Drawing.Size(100, 23);
             this.text3.TabIndex = 16;
             this.text3.TextChanged += new System.EventHandler(this.text4_TextChanged);
             // 
             // text4
             // 
-            this.text4.Location = new System.Drawing.Point(209, 399);
+            this.text4.Location = new System.Drawing.Point(241, 399);
             this.text4.Name = "text4";
-            this.text4.Size = new System.Drawing.Size(100, 20);
+            this.text4.Size = new System.Drawing.Size(100, 23);
             this.text4.TabIndex = 17;
             this.text4.TextChanged += new System.EventHandler(this.text5_TextChanged);
             // 
@@ -220,9 +224,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label1.Location = new System.Drawing.Point(280, 145);
+            this.label1.Location = new System.Drawing.Point(282, 141);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 25);
+            this.label1.Size = new System.Drawing.Size(315, 31);
             this.label1.TabIndex = 18;
             this.label1.Text = "Ļoti svarīga informācija!!!";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
@@ -231,17 +235,17 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label5.Location = new System.Drawing.Point(206, 170);
+            this.label5.Location = new System.Drawing.Point(205, 172);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(400, 16);
+            this.label5.Size = new System.Drawing.Size(507, 20);
             this.label5.TabIndex = 19;
             this.label5.Text = "Pirmo ievadiet preces daudzumu un pēc tam atzīmējiet pašu preci!";
             // 
             // text1
             // 
-            this.text1.Location = new System.Drawing.Point(209, 327);
+            this.text1.Location = new System.Drawing.Point(241, 328);
             this.text1.Name = "text1";
-            this.text1.Size = new System.Drawing.Size(100, 20);
+            this.text1.Size = new System.Drawing.Size(100, 23);
             this.text1.TabIndex = 14;
             this.text1.TextChanged += new System.EventHandler(this.text2_TextChanged);
             // 
@@ -250,7 +254,7 @@
             this.check5.AutoSize = true;
             this.check5.Location = new System.Drawing.Point(58, 422);
             this.check5.Name = "check5";
-            this.check5.Size = new System.Drawing.Size(148, 17);
+            this.check5.Size = new System.Drawing.Size(191, 21);
             this.check5.TabIndex = 21;
             this.check5.Text = "Vētrasputns 14,99 Eur/kg";
             this.check5.UseVisualStyleBackColor = true;
@@ -258,9 +262,9 @@
             // 
             // text6
             // 
-            this.text6.Location = new System.Drawing.Point(209, 422);
+            this.text6.Location = new System.Drawing.Point(241, 422);
             this.text6.Name = "text6";
-            this.text6.Size = new System.Drawing.Size(100, 20);
+            this.text6.Size = new System.Drawing.Size(100, 23);
             this.text6.TabIndex = 22;
             this.text6.TextChanged += new System.EventHandler(this.text6_TextChanged);
             // 
@@ -270,15 +274,15 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 23);
             this.button1.TabIndex = 23;
-            this.button1.Text = "Ierakstīšana failā";
+            this.button1.Text = "Izdrukāt čeku";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // vardaBox
             // 
-            this.vardaBox.Location = new System.Drawing.Point(371, 225);
+            this.vardaBox.Location = new System.Drawing.Point(392, 195);
             this.vardaBox.Name = "vardaBox";
-            this.vardaBox.Size = new System.Drawing.Size(100, 20);
+            this.vardaBox.Size = new System.Drawing.Size(100, 23);
             this.vardaBox.TabIndex = 24;
             this.vardaBox.TextChanged += new System.EventHandler(this.vardaBox_TextChanged);
             // 
@@ -286,7 +290,7 @@
             // 
             this.izmaksas1.Location = new System.Drawing.Point(371, 304);
             this.izmaksas1.Name = "izmaksas1";
-            this.izmaksas1.Size = new System.Drawing.Size(100, 20);
+            this.izmaksas1.Size = new System.Drawing.Size(100, 23);
             this.izmaksas1.TabIndex = 25;
             this.izmaksas1.TextChanged += new System.EventHandler(this.izmaksas1_TextChanged);
             // 
@@ -294,43 +298,71 @@
             // 
             this.izmaksas2.Location = new System.Drawing.Point(371, 327);
             this.izmaksas2.Name = "izmaksas2";
-            this.izmaksas2.Size = new System.Drawing.Size(100, 20);
+            this.izmaksas2.Size = new System.Drawing.Size(100, 23);
             this.izmaksas2.TabIndex = 26;
             // 
             // izmaksas3
             // 
             this.izmaksas3.Location = new System.Drawing.Point(371, 350);
             this.izmaksas3.Name = "izmaksas3";
-            this.izmaksas3.Size = new System.Drawing.Size(100, 20);
+            this.izmaksas3.Size = new System.Drawing.Size(100, 23);
             this.izmaksas3.TabIndex = 27;
             // 
             // izmaksas4
             // 
             this.izmaksas4.Location = new System.Drawing.Point(371, 375);
             this.izmaksas4.Name = "izmaksas4";
-            this.izmaksas4.Size = new System.Drawing.Size(100, 20);
+            this.izmaksas4.Size = new System.Drawing.Size(100, 23);
             this.izmaksas4.TabIndex = 28;
             // 
             // izmaksas5
             // 
             this.izmaksas5.Location = new System.Drawing.Point(371, 398);
             this.izmaksas5.Name = "izmaksas5";
-            this.izmaksas5.Size = new System.Drawing.Size(100, 20);
+            this.izmaksas5.Size = new System.Drawing.Size(100, 23);
             this.izmaksas5.TabIndex = 29;
             // 
             // izmaksas6
             // 
             this.izmaksas6.Location = new System.Drawing.Point(371, 422);
             this.izmaksas6.Name = "izmaksas6";
-            this.izmaksas6.Size = new System.Drawing.Size(100, 20);
+            this.izmaksas6.Size = new System.Drawing.Size(100, 23);
             this.izmaksas6.TabIndex = 30;
+            // 
+            // checkatlaide
+            // 
+            this.checkatlaide.AutoSize = true;
+            this.checkatlaide.Location = new System.Drawing.Point(321, 238);
+            this.checkatlaide.Name = "checkatlaide";
+            this.checkatlaide.Size = new System.Drawing.Size(188, 21);
+            this.checkatlaide.TabIndex = 31;
+            this.checkatlaide.Text = "Vai jums ir paldies karte?";
+            this.checkatlaide.UseVisualStyleBackColor = true;
+            this.checkatlaide.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // atlaidesbox
+            // 
+            this.atlaidesbox.Location = new System.Drawing.Point(685, 434);
+            this.atlaidesbox.Name = "atlaidesbox";
+            this.atlaidesbox.Size = new System.Drawing.Size(100, 23);
+            this.atlaidesbox.TabIndex = 32;
+            // 
+            // gala_summa
+            // 
+            this.gala_summa.Location = new System.Drawing.Point(371, 525);
+            this.gala_summa.Name = "gala_summa";
+            this.gala_summa.Size = new System.Drawing.Size(100, 23);
+            this.gala_summa.TabIndex = 33;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(871, 551);
+            this.ClientSize = new System.Drawing.Size(946, 630);
+            this.Controls.Add(this.gala_summa);
+            this.Controls.Add(this.atlaidesbox);
+            this.Controls.Add(this.checkatlaide);
             this.Controls.Add(this.izmaksas6);
             this.Controls.Add(this.izmaksas5);
             this.Controls.Add(this.izmaksas4);
@@ -400,6 +432,9 @@
         private System.Windows.Forms.TextBox izmaksas4;
         private System.Windows.Forms.TextBox izmaksas5;
         private System.Windows.Forms.TextBox izmaksas6;
+        private System.Windows.Forms.CheckBox checkatlaide;
+        private System.Windows.Forms.TextBox atlaidesbox;
+        private System.Windows.Forms.TextBox gala_summa;
     }
 }
 
--- EB_saldumi_11_G/Form1.cs
+++ EB_saldumi_11_G/Form1.cs
@@ -41,7 +41,10 @@ namespace EB_saldumi_11_G
         double Izmaksas4 = 0;
         double Izmaksas5 = 0;
         double Izmaksas6 = 0;
-
+        double atlaide = 1;
+        double gala_atlaide = 0;
+        double summa = 0;
+        double gala_cena = 0;
 
         public Form1()
         {
@@ -55,13 +58,42 @@ namespace EB_saldumi_11_G
 
         private void aprekins_Click(object sender, EventArgs e)
         {
+
+            if (checkatlaide.Checked == true)
+            {
+                atlaide = 0.13;
+                gala_atlaide = summa * atlaide;
+                atlaidesbox.Text = gala_atlaide.ToString();
+            }
+
             sk1 = Convert.ToDouble(daudzums.Text);
-            atlikums = sk1-(nauda0 + nauda1 + nauda2 + nauda3 + nauda4);
-             daudzums_kg.Text = atlikums.ToString();
-            //daudzums_kg.Text = daudzums_kg.ToString(); 
+            summa = nauda0 + nauda1 + nauda2 + nauda3 + nauda4 + nauda5;
+            gala_cena = summa - gala_atlaide;
+
+            gala_summa.Text = gala_cena.ToString();
+            atlikums = sk1 - gala_cena;
+            daudzums_kg.Text = atlikums.ToString();
             Izmaksas1 = kg0 * sk2;
-            Izmaksas1 = Convert.ToDouble(izmaksas1.Text);
+            Izmaksas2 = sk3 * kg1;
+            Izmaksas3 = sk4 * kg2;
+            Izmaksas4 = sk5 * kg3;
+            Izmaksas5 = sk6 * kg4;
+            Izmaksas6 = sk7 * kg5;
+
+
             izmaksas1.Text = Izmaksas1.ToString();
+            izmaksas2.Text = Izmaksas2.ToString();
+            izmaksas3.Text = Izmaksas3.ToString();
+            izmaksas4.Text = Izmaksas4.ToString();
+            izmaksas5.Text = Izmaksas5.ToString();
+            izmaksas6.Text = Izmaksas6.ToString();
+
+            if (atlikums < 0)
+            {
+                MessageBox.Show("Jums nav pietiekami daudz naudas!!!");
+                Application.Exit();
+            }
+        
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -199,19 +231,95 @@ namespace EB_saldumi_11_G
             {
                 sk7 = 14.99;
                 skaits = skaits + 1;
-                kg5 = Convert.ToDouble(text2.Text);
-                nauda2 = kg5 * sk7;
+                kg5 = Convert.ToDouble(text6.Text);
+                nauda5 = kg5 * sk7;
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int ceka_Nr = 0;
             string vards = this.vardaBox.Text;
-            string failanosaukums = vards + "_" + DateTime.Now.ToString("ddMMyyyy") + "_ceks.txt";
-            StreamWriter sw = new StreamWriter(failanosaukums);
-            sw.WriteLine(sk1);
-            sw.WriteLine(atlikums);
+            string failanosaukums = vards + ceka_Nr + "_" + DateTime.Now.ToString("ddMMyyyy") + "_ceks.txt";
 
+            if (File.Exists(failanosaukums))
+            {
+                ceka_Nr = ceka_Nr + 1;
+                failanosaukums = vards + ceka_Nr + "_" + DateTime.Now.ToString("ddMMyyyy") + "_ceks.txt";
+            }
+
+
+            StreamWriter sw = new StreamWriter(failanosaukums);
+
+            sw.WriteLine("SIA <<RIMI>>");
+            sw.WriteLine(" ");
+           
+            if (text0.Text.Equals(" "))
+            {
+                MessageBox.Show("Aizpildiet šo lauku!");
+            }
+            else
+            {
+                sw.WriteLine("Marmelāde");
+                sw.WriteLine(kg0 + "kg x " + sk2 + "Eur = " + nauda0 + "Eur");
+            }
+
+            if (text1.Text.Equals(" "))
+            {
+                MessageBox.Show("Aizpildiet šo lauku!");
+            }
+            else
+            {
+                sw.WriteLine("Konfektes serenāde");
+                sw.WriteLine(kg1 + "kg x " + sk3 + "Eur = " + nauda1 + "Eur");
+            }
+
+            if (text2.Text.Equals(" "))
+            {
+                MessageBox.Show("Aizpildiet šo lauku!");
+            }
+            else
+            {
+                sw.WriteLine("Konfektes Migle");
+                sw.WriteLine(kg2 + "kg x " + sk4 + "Eur = " + nauda2 + "Eur");
+            }
+
+            if (text3.Text.Equals(" "))
+            {
+                MessageBox.Show("Aizpildiet šo lauku!");
+            }
+            else
+            {
+                sw.WriteLine("Konfektes Rudzupuķe");
+                sw.WriteLine(kg3 + "kg x " + sk5 + "Eur = " + nauda3 + "Eur");
+            }
+
+            if (text4.Text.Equals(" "))
+            {
+                MessageBox.Show("Aizpildiet šo lauku!");
+            }
+            else
+            {
+                sw.WriteLine("Konfektes Gotiņa");
+                sw.WriteLine(kg4 + "kg x " + sk6 + "Eur = " + nauda4 + "Eur");
+            }
+
+            if (text6.Text.Equals(" "))
+            {
+                MessageBox.Show("Aizpildiet šo lauku!");
+            }
+            else
+            {
+                sw.WriteLine("Konfektes Vētrasputns");
+                sw.WriteLine(kg5 + "kg x " + sk7 + "Eur = " + nauda5 + "Eur");
+            }
+
+            sw.WriteLine(" ");
+            sw.WriteLine("Samaksai Eur " + summa);
+            sw.WriteLine("Iegūtā atlaide Eur " + gala_atlaide);
+            sw.WriteLine("Samaksai pēc atlaides Eur " + gala_cena);
+            sw.WriteLine("Saņemtā nauda Eur " + sk1);
+            sw.WriteLine("Izdotā nauda Eur " + atlikums);
             sw.Close();
         }
 
@@ -239,6 +347,20 @@ namespace EB_saldumi_11_G
             
 
 
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkatlaide.Checked == true)
+            {
+                atlaide = 0.13;
+                gala_atlaide = sk1 * atlaide;
+            }
         }
     }
 }
